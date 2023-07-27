c402f67bb69aff7e9313c63a3aad84098892b9bd
kunal raj shah <rajkunal7279@gmail.com>
2023-07-28 00:38:55 +0530

    Adding message when 0 and 1 Expenses are there

--- first-react-project/src/Components/All Expenses Rendering/Expenses.js
+++ first-react-project/src/Components/All Expenses Rendering/Expenses.js
@@ -1,25 +1,52 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Expenseitem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
-import ExpenseFilter from './ExpensesFilter';
+import ExpenseFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-    const [filteredYear,setFilteredYear]=useState('2020');
-    const filterChangeHandler=(selectedYear)=>{
-      setFilteredYear(selectedYear);
-    };
+  // For Display Filter Expenses
+  const filterExpenses = props.items.filter((exp) => {
+    return exp.date.getFullYear().toString() === filteredYear;
+  });
 
-    // For Display Filter Expenses
-    const filterExpenses=props.items.filter((exp)=>{
-      return exp.date.getFullYear().toString() === filteredYear;
-    });
+  // We can use HTML element inside the variable in JSX code.
+  let noExpenseMsg=<p>No Expense Found !!</p>
+  let oneExpenseMsg=<p>Only single Expense here. Please add more...</p>
 
-    return (
-      <Card className="Expenses">
-        <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-        {filterExpenses.map((expense) => (
+  if(filterExpenses.length > 0){
+    noExpenseMsg=filterExpenses.map((expense) => (
+      // When we rander list always add unique ID.
+      <Expenseitem
+        key={expense.id}
+        date={expense.date}
+        title={expense.title}
+        amount={expense.amount}
+      />
+    ))
+  }
+  
+
+  return (
+    <Card className="Expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {/* use Ternary Opeator to rander filterExpense. Bcz, inside{} we can't use if or for loop*/}
+      {/* We can also return by && property of js */}
+      {/* {filterExpenses.length === 0 && <p>No Expense Found.</p>} */}
+      {/* it check if first condition is true then it return second element */}
+    
+      {/* {filterExpenses.length === 0 ? (
+        <p>No Expense Found.</p>
+      ) : (
+        filterExpenses.map((expense) => (
           // When we rander list always add unique ID.
           <Expenseitem
             key={expense.id}
@@ -27,9 +54,14 @@ const Expenses = (props) => {
             title={expense.title}
             amount={expense.amount}
           />
-        ))}
-      </Card>
-    );
+        ))
+      )} */}
+
+      {noExpenseMsg}
+      {filterExpenses.length ===1 && oneExpenseMsg}
+
+    </Card>
+  );
 };
 
 export default Expenses;
