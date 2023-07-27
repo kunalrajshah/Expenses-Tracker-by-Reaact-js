06ed0d90d742a8259299c603bc2d00c7e97c62e1
kunal raj shah <rajkunal7279@gmail.com>
2023-07-27 17:38:11 +0530

    Adding key on Rendering List

--- first-react-project/src/Components/All Expenses Rendering/Expenses.js
+++ first-react-project/src/Components/All Expenses Rendering/Expenses.js
@@ -1,13 +1,23 @@
+import React,{useState} from "react";
 import Expenseitem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpenseFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
-  {
+
+    const [filteredYear,setFilteredYear]=useState('2020');
+    const filterChangeHandler=(selectedYear)=>{
+      setFilteredYear(selectedYear);
+    }
+
     return (
       <Card className="Expenses">
+        <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
         {props.items.map((expense) => (
+          // When we rander list always add unique ID.
           <Expenseitem
+            key={expense.id}
             date={expense.date}
             title={expense.title}
             amount={expense.amount}
@@ -15,7 +25,6 @@ const Expenses = (props) => {
         ))}
       </Card>
     );
-  }
 };
 
 export default Expenses;
